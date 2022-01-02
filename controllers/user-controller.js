2876e8d4e24b131cbc03b6a01f1924b8b0666453
leena <annachandy123@gmail.com>
2022-01-02 15:14:06 +1100

    controllers added

---
+++ controllers/user-controller.js
@@ -0,0 +1,104 @@
+const { User } = require("../models");
+
+const userController = {
+    // get all users also the callback function for api route
+    getAllUser(req, res) {
+        User.find({})
+            // populates the mongodb field with data
+            .populate({
+                path: "thoughts",
+                // this ignores __v
+                select: "-__v",
+            })
+            .select("-__v")
+            .sort({ _id: -1 })
+            .then((dbUserData) => res.json(dbUserData))
+            .catch((err) => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    },
+
+    // get user by id
+    getUserById({ params }, res) {
+        // instead of acccessing the entire req, aparams is destructured out of it
+        User.findOne({ _id: params.id })
+            .populate({
+                path: "thoughts",
+                select: "-__v",
+            })
+            .select("-__v")
+            .then((dbUserData) => {
+                if (!dbUserData) {
+                    res.status(404).json({ message: `No user found with the id of ${req.params.id}` });
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch((err) => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    },
+
+    // create User by destructuring the body out of the express req object
+    createUser({ body }, res) {
+        User.create(body)
+            .then((dbUserData) => res.json(dbUserData))
+            .catch((err) => res.status(400).json(err));
+    },
+
+    // update User by id
+    updateUser({ params, body }, res) {
+        User.findOneAndUpdate({ _id: params.id }, body, {
+            new: true,
+        })
+            .then((dbUserData) => {
+                if (!dbUserData) {
+                    res.status(404).json({ message: `No user found with the id of ${req.params.id}` });
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch((err) => res.status(400).json(err));
+    },
+
+    // delete User
+    deleteUser({ params }, res) {
+        User.findByIdAndDelete({ _id: params.id })
+            .then((dbUserData) => {
+                if (!dbUserData) {
+                    res.status(404).json({ message: `No user found with the id of ${req.params.id}` });
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch((err) => res.status(400).json(err));
+    },
+
+    // add friend
+    addFriend({ params }, res) {
+        User.findOneAndUpdate(
+            { _id: params.id },
+            { $push: { friends: params.friendsId } }, // parameter containing friendsId
+            { new: true }
+        )
+            .then((dbUserData) => res.json(dbUserData))
+            .catch((err) => res.status(400).json(err));
+    },
+
+    // delete friend
+    deleteFriend({ params }, res) {
+        User.findOneAndUpdate({ _id: params.id }, { $pull: { friends: params.friendsId } }, { new: true })
+            .then((dbUserData) => {
+                if (!dbUserData) {
+                    res.status(404).json({ message: `No user found with the id of ${req.params.id}` });
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch((err) => res.status(400).json(err));
+    },
+};
+
+module.exports = userController;

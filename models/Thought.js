097e2c4eab546a55627be5bac9a3a2458aa7aafc
leena <annachandy123@gmail.com>
2022-01-02 15:33:12 +1100

    models updated

--- models/Thought.js
+++ models/Thought.js
@@ -56,7 +56,7 @@ const thoughtSchema = new Schema(
     }
 );
 thoughtSchema.virtual("reactionCount").get(function () {
-    return this.reaction.length;
+    return this.reactions.length;
 });
 const Thought = model("Thought", thoughtSchema);
 module.exports = Thought;
